cb593bb74156278c302e63fff6b9b681b656cf1e
Carlos Martinez <carlos.martinez@medwing.com>
2026-02-15 21:17:40 +0100

    first stage

--- main.go
+++ main.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var hadError bool = false
 
 func main() {
-	fmt.Println("Hello, World!")
+	if len(os.Args) > 3 {
+		fmt.Println("Usage: glox <script>")
+		os.Exit(64)
+	} else if len(os.Args) == 2 {
+		runFile(os.Args[1])
+	} else {
+		runInteractive()
+	}
+}
+
+func runFile(filename string) {
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
+		os.Exit(1)
+	}
+
+	run(string(content))
+
+	if hadError {
+		os.Exit(65)
+	}
+}
+
+func runInteractive() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("> ")
+		if !scanner.Scan() {
+			break
+		}
+
+		line := scanner.Text()
+		run(line)
+		hadError = false
+	}
+}
+
+func run(line string) {
+	// scanner := NewScanner(line)
+	// tokens := scanner.ScanTokens()
+	//
+	// for _, token := range tokens {
+	// 	fmt.Printf("%s %s\n", token.Type, token.Literal)
+	// }
+}
+
+func error(line int, message string) {
+	fmt.Printf("Error: %s [line %d]\n", message, line)
+	hadError = true
 }
